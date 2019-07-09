075290d64787c1ad5c084af7190738bd3f651318
Brandon <47625174+Brandon097@users.noreply.github.com>
2019-07-09 15:08:31 -0500

    Update You been summoned.js

--- You been summoned.js
+++ You been summoned.js
@@ -1,57 +1,55 @@
-// Begin
+      // Begin
       function GetCookie (name) {
-      var arg = name + "=";
-      var alen = arg.length;
-      var clen = document.cookie.length;
-      var i = 0;
+      const arg = name + "=";
+      const alen = arg.length;
+      const clen = document.cookie.length;
+      let i = 0;
       while (i < clen) {
-      var j = i + alen;
-      if (document.cookie.substring(i, j) == arg)
-      return getCookieVal (j);
+      const j = i + alen;
+      if (document.cookie.substring(i, j) === arg) return getCookieVal(j);
       i = document.cookie.indexOf(" ", i) + 1;
-      if (i == 0) break;
+      if (i === 0) break;
       }
       return null;
     }
       function SetCookie (name, value) {
-      var argv = SetCookie.arguments;
-      var argc = SetCookie.arguments.length;
-      var expires = (argc > 2) ? argv[2] : null;
-      var path = (argc > 3) ? argv[3] : null;
-      var domain = (argc > 4) ? argv[4] : null;
-      var secure = (argc > 5) ? argv[5] : false;
-      document.cookie = name + "=" + escape (value) +
-      ((expires == null) ? "" : ("; expires=" + expires.toGMTString())) +
-      ((path == null) ? "" : ("; path=" + path)) +
-      ((domain == null) ? "" : ("; domain=" + domain)) +
-      ((secure == true) ? "; secure" : "");
+      const argv = SetCookie.arguments;
+      const argc = SetCookie.arguments.length;
+      const expires = argc > 2 ? argv[2] : null;
+      const path = argc > 3 ? argv[3] : null;
+      const domain = argc > 4 ? argv[4] : null;
+      const secure = argc > 5 ? argv[5] : false;
+      document.cookie = name + "=" + escape(value) +
+      (expires === null ? "" : "; expires=" + expires.toGMTString()) +
+      (path === null ? "" : "; path=" + path) +
+      (domain === null ? "" : "; domain=" + domain) +
+      (secure === true ? "; secure" : "");
     }
       function DeleteCookie (name) {
-      var exp = new Date();
-      exp.setTime (exp.getTime() - 1);
-      var cval = GetCookie (name);
+      const exp = new Date();
+      exp.setTime(exp.getTime() - 1);
+      const cval = GetCookie(name);
       document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
     }
-      var expDays = 30;
-      var exp = new Date();
-      exp.setTime(exp.getTime() + (expDays*24*60*60*1000));
-      function amt(){
-      var count = GetCookie('count')
-      if(count == null) {
-      SetCookie('count','1')
-      return 1
+      const expDays = 30;
+      const exp = new Date();
+      exp.setTime(exp.getTime() + expDays * 24 * 60 * 60 * 1000);
+      function amt () {
+      const count = GetCookie('count');
+      if (count === null) {
+      SetCookie('count', '1');
+      return 1;
     }
-      else {
-      var newcount = parseInt(count) + 1;
-      DeleteCookie('count')
-      SetCookie('count',newcount,exp)
-      return count
+      
+      const newcount = parseInt(count) + 1;
+      DeleteCookie('count');
+      SetCookie('count', newcount, exp);
+      return count;
+    
     }
-    }
-      function getCookieVal(offset) {
-      var endstr = document.cookie.indexOf (";", offset);
-      if (endstr == -1)
-      endstr = document.cookie.length;
+      function getCookieVal (offset) {
+      let endstr = document.cookie.indexOf(";", offset);
+      if (endstr === -1) endstr = document.cookie.length;
       return unescape(document.cookie.substring(offset, endstr));
-      }
+    }
       // End
